d0df56f8c5e050194d55e1dc2e4373c0a777e200
TorasHill <toras.hill96@gmail.com>
2026-02-08 13:36:15 -0600

    add modal functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,28 @@
-const initialCards = [{
-  name: "Val Thorens",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
-},
-{
-  name: "Restaurant terrace",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
-},
-{
-  name: "An outdoor cafe",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
-},
-{
-  name: "A very long bridge, over the forest and through the trees",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
-},
-{
-  name: "Tunnel with morning light",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
-},
-{
-  name: "Mountain house",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
-},
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
 ];
 
 const editProfileBtn = document.querySelector(".profile__edit-btn");
@@ -31,9 +32,14 @@ const editProfileForm = editProfileModal.querySelector(".modal__form");
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
-const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input",
+);
 
-const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input",
+);
+
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
 }
@@ -61,16 +67,15 @@ const newPostNameInput = newPostModal.querySelector("#card-name-input");
 const newPostLinkInput = newPostModal.querySelector("#card-image-input");
 
 function handleAddCardSubmit(evt) {
-
   evt.preventDefault();
 
-console.log(newPostNameInput.value);
-console.log(newPostLinkInput.value);
+  console.log(newPostNameInput.value);
+  console.log(newPostLinkInput.value);
 
-closeModal(newPostModal);
+  closeModal(newPostModal);
 }
 
-newPostFormElement.addEventListener('submit', handleAddCardSubmit);
+newPostFormElement.addEventListener("submit", handleAddCardSubmit);
 newPostBtn.addEventListener("click", function () {
   openModal(newPostModal);
 });
@@ -89,6 +94,6 @@ function handleEditProfileFormSubmit(evt) {
 editProfileForm.addEventListener("submit", handleEditProfileFormSubmit);
 
 initialCards.forEach(function (item) {
-   console.log(item.name);
-   console.log(item.link);
+  console.log(item.name);
+  console.log(item.link);
 });
